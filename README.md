5f809b7570eace518090d9c725e1b07a7af52c57
sugikeitter <sugikeitter@yahoo.co.jp>
2022-05-07 23:40:15 +0900

    Add insert error handling

--- README.md
+++ README.md
@@ -23,6 +23,7 @@ uvicorn main:app --host 0.0.0.0
 
 
 ```sql
+-- テーブル定義とサンプルデータINSERT
 CREATE TABLE books (
     book_id SERIAL PRIMARY KEY,
     title VARCHAR ( 100 ) NOT NULL,
@@ -45,4 +46,45 @@ VALUES (
     current_timestamp,
     current_timestamp
 );
+```
+
+`EC2 Amazon Linux 2 のユーザーデータ`
+```bash
+#!/bin/bash
+
+yum update -y
+yum install git gcc -y
+amazon-linux-extras install python3.8 postgresql12 -y
+ln -fs /usr/bin/python3.8 /usr/bin/python3
+
+sudo -u ec2-user python3 -m pip install pip --upgrade
+sudo -u ec2-user python3 -m pip install wheel fastapi uvicorn[standard] boto3 Jinja2 sqlalchemy psycopg2-binary python-multipart
+
+sudo -u ec2-user git clone https://github.com/sugikeitter/FastApi-Jinja-SQLAlchemy.git /home/ec2-user/FastApi-Jinja-SQLAlchemy/
+sudo -u ec2-user mkdir /home/ec2-user/log
+
+# systemdの設定
+cat <<EOF > /etc/systemd/system/fastapi.service
+[Unit]
+Description=fastapi
+After=network-online.target
+
+[Service]
+EnvironmentFile=/etc/sysconfig/fastapi_env
+User=ec2-user
+Group=ec2-user
+WorkingDirectory=/home/ec2-user/FastApi-Jinja-SQLAlchemy
+ExecStart=/bin/python3 -m uvicorn main:app --host 0.0.0.0
+ExecStop=/bin/kill -s TERM $MAINPID
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# $YOUR_PARAM は書き換え
+cat <<EOF > /etc/sysconfig/fastapi_env
+RDS_SECRET_ID='$YOUR_PARAM'
+EOF
+
+systemctl start fastapi
 ```
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi import Depends, FastAPI, Form, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.exc import InternalError
 from sqlalchemy.orm import Session
 
 import const
@@ -61,7 +62,38 @@ async def post(request: Request,
             }
         )
 
-    rdb_crud.insert_books(db, title)
+    try:
+        rdb_crud.insert_books(db, title)
+    except InternalError as e:
+        db.rollback()
+        books = rdb_crud.get_books(db)
+        timestamp = datetime.now(const.JST).isoformat()[0:23]  # 日本時間のミリ秒3桁までの文字列
+        return templates.TemplateResponse(
+            "index.html",
+            {
+                "request": request,
+                "aws_az": const.AWS_AZ,
+                "private_ip": const.PRIVATE_IP,
+                "error_msg": f"データの更新に失敗しました ☠️ 原因: {e.orig}",
+                "timestamp": timestamp,
+                "books": books,
+            }
+        )
+    except Exception as e:
+        db.rollback()
+        books = rdb_crud.get_books(db)
+        timestamp = datetime.now(const.JST).isoformat()[0:23]  # 日本時間のミリ秒3桁までの文字列
+        return templates.TemplateResponse(
+            "index.html",
+            {
+                "request": request,
+                "aws_az": const.AWS_AZ,
+                "private_ip": const.PRIVATE_IP,
+                "error_msg": f"データの更新に失敗しました ☠️ 原因: {e}",
+                "timestamp": timestamp,
+                "books": books,
+            }
+        )
     # INSERTが成功したらトップへリダイレクト、status_codeなしだとpostリクエストのループになる
     return RedirectResponse("/", status_code=302)
 
